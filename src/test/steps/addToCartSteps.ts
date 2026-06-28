ab9aeb9c2fc75da636cce0872559a809d027ebff
lokichandu4 <lokichandu4@gmail.com>
2026-06-28 08:50:35 +0530

    Intial commit

---
+++ src/test/steps/addToCartSteps.ts
@@ -0,0 +1,20 @@
+
+import { Given, When, Then, Before, After } from "@cucumber/cucumber";
+import { chromium, Page, Browser, expect } from "@playwright/test";
+
+let browser: Browser;
+let page!: Page;
+
+Given('user search for a {string}', async function (book) {
+  await page.locator("input[type='search']").type(book);
+  await page.locator("mat-option[role='option'] span").click();
+});
+
+When('user add the book to the cart', async function () {
+  await page.locator("//button[@color='primary']").click();
+});
+
+Then('the cart badge should get updated', async function () {
+  const badgeCount = await page.locator("#mat-badge-content-0").textContent();
+  expect(Number( badgeCount?.length)).toBeGreaterThan(0);
+});
